46c21efcc57ccb8b96ead142156a2e190ec855e9
Alexander Constanti <Alex.Constanti@coexservices.com.au>
2022-09-16 11:47:49 +1000

    SPLT-438: Add validate_commit_msg hook.

---
+++ hooks/validate_commit_msg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ARGS=$1
+COMMIT_MSG=$(head -n1 "$ARGS") # Get the first line of a multiline commit msg
+PATTERN="^[A-Z][A-Z0-9]+-[0-9]+: .*" # Format of 'JIRA1-123: ...'
+
+if ! [[ "$COMMIT_MSG" =~ $PATTERN ]]; then
+    cat "$COMMIT_MSG"
+    echo "Your commit message must follow the format of 'JIRA-123: ' Did you forget to add your jira?"
+    echo
+    echo "Please follow this format so Jira can auto detect what is in each release."
+    echo "The regex is '$PATTERN'"
+    exit 1
+fi
